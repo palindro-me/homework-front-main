adce22ab613e3045c01d22292c64ced4bfd43fa7
Anna Kazak <anna.s.kazak@gmail.com>
2025-05-27 14:29:27 +0200

    HW was done

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import './App.css';
+import React from 'react';
+import {HW3} from './HW/3/HW3';
 
 function App() {
   return (
     <div className="App">
       {/*<HW1 />*/}
       {/*<HW2 />*/}
-      {/*<HW3 />*/}
-      <div>hhh</div>
-      <div>hhh</div>
+      <HW3 />
       {/*<HW4 />*/}
     </div>
   );
